3a7246b4af663804c6bac8f722d5366044830143
Devon Meunier <devon.meunier@utoronto.ca>
2012-06-10 22:34:05 -0400

    Initial commit.

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(name="romexpander",
+      version="0.4",
+      author="Devon Meunier",
+      author_email="devon.meunier@utoronto.ca",
+      license="MIT",
+      scripts=["romexpander"],
+      description="NES ROM Expansion script compatible with DvD's " \
+                  "ROM Expander Pro.",
+      long_description=open("README").read(),
+      install_requires=[
+          "docopt>=0.3.0",
+      ],
+)
