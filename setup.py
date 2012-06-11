7ba654b4ba19231488049a5994cbbd0d6d9cf9de
Devon Meunier <devon.meunier@utoronto.ca>
2012-06-10 22:42:46 -0400

    Added url metadata to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(name="romexpander",
       version="0.4",
       author="Devon Meunier",
       author_email="devon.meunier@utoronto.ca",
+      url="http://www.cdf.utoronto.ca/~g8m/",
       license="MIT",
       scripts=["romexpander.py"],
       description="NES ROM Expansion script compatible with DvD's " \
