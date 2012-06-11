8d1e1380f9e93d24ae192f24c3b7ffe5a7d19d6d
Devon Meunier <devon.meunier@utoronto.ca>
2012-06-10 22:41:04 -0400

    Minor packaging changes.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(name="romexpander",
       author="Devon Meunier",
       author_email="devon.meunier@utoronto.ca",
       license="MIT",
-      scripts=["romexpander"],
+      scripts=["romexpander.py"],
       description="NES ROM Expansion script compatible with DvD's " \
                   "ROM Expander Pro.",
       long_description=open("README").read(),
